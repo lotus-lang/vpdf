6c2cad3f75f94f1a9119a0a1e81e553c280c1268
Tazmilur Saad <ssaad@colgate.edu>
2023-02-05 19:05:45 -0500

    Fixed compress function in Lzw.

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,5 @@
-let x = Vpdf.Lzw.compress ["A";"B";"B";"A";"B";"B";"B";"A";"B";"B";"A"] in
-  List.iter (Printf.printf "%d ") (List.rev x)
+
+let y = List.map Char.chr [45;45;45;45;45;65;45;45;45;66] in
+let x = Vpdf.Lzw.compress (String.to_seq (String.of_seq (List.to_seq y))) in
+List.iter (Printf.printf "%d ") (List.rev x);
+Printf.printf "\n"
--- lib/lzw.ml
+++ lib/lzw.ml
@@ -1,26 +1,38 @@
-module Tbl = struct 
-  include Map.Make(String)
+module Tbl = struct
+  include Map.Make (String)
 
-  let default = 
+  let default =
     let e = empty in
     let l = List.init 255 (fun x -> x + 1) in
-    let rec create tbl lst = match lst with
+    let rec create tbl lst =
+      match lst with
       | h :: t -> create (add (String.make 1 (Char.chr h)) h tbl) t
       | [] -> tbl
-  in create e l
+    in
+    create e l
 end
 
-let compress seq =
-    let m = Tbl.default in
-    let rec helper tbl lst cur out = match lst with
-      | [] -> (Tbl.find cur tbl) :: out
-      | h :: t -> 
-        let ncur = cur ^ h in match (Tbl.find_opt ncur tbl) with
-          | None -> 
-            let ntbl = Tbl.add ncur ((Tbl.cardinal tbl) + 1) tbl in
-            let nout = (Tbl.find cur tbl) :: out in
-            helper ntbl t h nout
-          | Some _ -> helper tbl t ncur out
-    in helper m seq "" []
-
+let compress s =
+  let eod = 257 in
+  let clear_table = 256 in
+  let maxCard = 4095 in
+  let m = Tbl.default in
+  let rec helper tbl seq cur out =
+    match Seq.uncons seq with
+    | None -> eod :: (Tbl.find cur tbl :: out)
+    | Some (h, t) -> (
+        let ncur = cur ^ String.make 1 h in
+        match Tbl.find_opt ncur tbl with
+        | None ->
+            (* Adds 3 since EOD marker is 257*)
+            let ncode = Tbl.cardinal tbl + 3 in
+            let ntbl =
+              if ncode <= maxCard then Tbl.add ncur ncode tbl else tbl
+            in
+            let nout = Tbl.find cur tbl :: out in
+            helper ntbl t (String.make 1 h) nout
+        | Some _ -> helper tbl t ncur out)
+  in
+  (* By convention, the encoder begins by issuing a clear-table code. *)
+  helper m s "" [clear_table]
 
